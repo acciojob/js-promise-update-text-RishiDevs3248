df4550db2ce8e9490d88b552c62d45a44df7baf9
Hrishikesh Alabnur <144805491+RishiDevs3248@users.noreply.github.com>
2025-10-27 16:46:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+// your JS code here
+let output = document.getElementById('output');
+
+function xyz() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+xyz()
+  .then((message) => {
+    output.textContent = message;
+  })
+  .catch((err) => {
+    console.error(err);
+  });
